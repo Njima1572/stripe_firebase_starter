e24042f59f163a1c3d7e4c66ab296b4d694ad914
Njima1572 <kochi.nakajima@gmail.com>
2021-08-28 14:20:38 -0400

    This is a react version

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { hmr: { host: "localhost", port: 3200 } }, // Using different port from the actual port bc docker-compose
-  plugins: [vue()],
-});
+  plugins: [reactRefresh()]
+})
